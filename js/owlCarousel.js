2de0fd7a9398774ecb1ab4cb430489ae533cba1f
Dima Home Desctop <gldmvl@gmail.com>
2018-04-08 17:44:27 +0300

    Fixed scrool homt title bottom

--- js/owlCarousel.js
+++ js/owlCarousel.js
@@ -7,63 +7,60 @@ $.cj.owlCarousel = {
 
         var owl = $('.owl-carousel');
 
-        owl.owlCarousel({
-            nav: false,
-            loop: true,
-            responsiveClass: true,
-            responsive: {
-                0: {
-                    items: 1,
-                    nav: false
-                },
-                720: {
-                    items: 2,
-                    nav: false
-                },
-                1000: {
-                    items: 3,
-                    nav: false
+        if(owl.length != 0) {
+            owl.owlCarousel({
+                nav: false,
+                loop: true,
+                responsiveClass: true,
+                responsive: {
+                    0: {
+                        items: 1,
+                        nav: true
+                    },
+                    720: {
+                        items: 2,
+                        nav: false
+                    },
+                    1000: {
+                        items: 3,
+                        nav: true
+                    }
                 }
-            }
-        });
+            });
 
-        $('.partners-next').on('click', function () {
-            owl.trigger('next.owl.carousel');
-        });
+            $('.partners-next').on('click', function () {
+                owl.trigger('next.owl.carousel');
+            });
 
-        $('.partners-prev').on('click', function () {
-            owl.trigger('prev.owl.carousel');
-        });
+            $('.partners-prev').on('click', function () {
+                owl.trigger('prev.owl.carousel');
+            });
+        }
 
-        var owl2 = $('.slider-clients');
+        var sliderClients = $('#slider-clients');
 
-        owl2.owlCarousel({
-            nav: false,
-            loop: true,
-            responsiveClass: true,
-            responsive: {
-                0: {
-                    items: 1,
-                    nav: false
-                },
-                720: {
-                    items: 2,
-                    nav: false
-                },
-                1000: {
-                    items: 3,
-                    nav: false
+        if(sliderClients.length != 0)
+        {
+            owl.owlCarousel({
+                nav: false,
+                loop: true,
+                responsiveClass: true,
+                responsive: {
+                    0: {
+                        items: 1,
+                        nav: false
+                    },
+                    720: {
+                        items: 2,
+                        nav: false
+                    },
+                    1000: {
+                        items: 3,
+                        nav: false
+                    }
                 }
-            }
-        });
-
-        $('.partners-next').on('click', function () {
-            owl2.trigger('next.owl.carousel');
-        });
-        
-        $('.partners-prev').on('click', function () {
-            owl2.trigger('prev.owl.carousel');
-        });
+            });
+        }
     }
 };
 
